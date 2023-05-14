d061a85815e870ed218db7edef8710efb2f1b9f8
Amilharshak <amilvk125@gmail.com>
2023-05-14 21:26:32 +0530

    created python file for oop

--- oop.py
+++ oop.py
@@ -4,3 +4,20 @@ class students():
         self.name = ""
         self.age = 0
 
+    def register(self,name,age):
+        self.name = name
+        self.age = age
+        return "ok"
+
+
+
+amil = students()
+
+akhil = students()
+
+amil.register("amil",50)
+
+akhil.register("akhil",40)
+
+print(amil.name,amil.age)
+print(akhil.name,akhil.age)
